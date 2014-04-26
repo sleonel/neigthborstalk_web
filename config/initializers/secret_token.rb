9804994bc97ff0c5cf86696681e19a58a30ab076
Leonel Suárez <sleonel@uninorte.edu.co>
2014-04-26 13:09:25 -0400

    creacion del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NeigthborstalkWeb::Application.config.secret_token = '5aab09a0e2e7e2a245d6de90cdfb43caea9904b92114eaa1f0fb8327c86ba25a803c65a54753f8e020b0ae9f02e2b322c84e15e589003c2919693b26aca66c5a'
